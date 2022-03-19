ba2a7e6bb2fb0a6ae3cd5a6e060564a105aab90f
AllanyGFilgueiras <allanyfilgueiras@gmail.com>
2022-03-19 17:12:22 -0300

    corpo da terceira tela

--- src/views/CuidadosCovid.vue
+++ src/views/CuidadosCovid.vue
@@ -1,6 +1,8 @@
 <template>
     <v-container>
-        <h1>Covid</h1>
+        <h2 class="text-h5 text-center mb-0 mt-5"><strong>Covid19</strong></h2>
+        <p class="text-home text-justify mt-2 ml-4 mr-4"><strong>Use máscara em público, especialmente em locais fechados ou quando não for possível manter o distanciamento físico.
+        Prefira locais abertos e bem ventilados em vez de ambientes fechados. Abra uma janela se estiver em um local fechado.</strong></p>  
     </v-container>
 </template>
 
