acea9d42917506ed13d5bf0c300eafbdb6ac8425
Shenon Oliveira e Silva <shenonsilva@gmail.com>
2022-03-19 12:08:04 -0300

    Mudanças na App Top Bar Router

--- src/components/AppTopBar.vue
+++ src/components/AppTopBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-top-bar">
     <v-app-bar color="deep-purple accent-4" dense dark>
-      <v-app-bar-nav-icon @click="drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
       <v-toolbar-title>
         <router-link class="MeuInicio" to="/">
@@ -14,22 +14,34 @@
       <v-list nav dense>
         <v-list-item-group
           v-model="group"
-          active-class="deep-purple--text text--accent-4">
+          active-class="deep-purple--text text--accent-4"
+        >
+          <router-link to="/">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Início</v-list-item-title>
+            </v-list-item>
+          </router-link>
 
-          
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
+          <router-link to="/locais">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Locais</v-list-item-title>
+            </v-list-item>
+          </router-link>
 
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Account</v-list-item-title>
-          </v-list-item>
+          <router-link to="/covid">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Orientações Covid</v-list-item-title>
+            </v-list-item>
+          </router-link>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
@@ -47,4 +59,7 @@ export default {
 </script>
 
 <style scoped>
+.app-top-bar a {
+  text-decoration: none;
+}
 </style>
