4e6d5aab201ef3e84eebd2c177b744ef628d15fd
AllanyGFilgueiras <allanyfilgueiras@gmail.com>
2022-03-19 16:33:48 -0300

    alteração no inicio

--- src/views/MeuInicio.vue
+++ src/views/MeuInicio.vue
@@ -3,7 +3,7 @@
         <div class="mt-0git inicio-img">
             <img src="../assets/logoInicio.svg" alt="Pessoas no Carmaval">
             </div>
-        <h2 class="text-h5 text-center mb-3 mt-5"><strong>Carnaval na Pandemia</strong></h2>
+        <h2 class="text-h5 text-center mb-0 mt-5"><strong>Carnaval na Pandemia</strong></h2>
         <p class="text-home text-justify mt-2 ml-4 mr-4">A Fulia não pode parar! Os blocos estão respeitando as regras sanitárias, por isso, até nos encontrarmos aproveite a festa em casa.
         Enquanto você curte seus drinks e as lives, conheça um pouco mais sobre os blocos carnavalescos do Brasil!! Compartilhe com família e amigos! Prepare a sua lista para vir conhecer essa tradição fenomenal!</p>  
     </v-container>
